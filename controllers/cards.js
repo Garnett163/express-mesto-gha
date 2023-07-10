316e8726703c95ce27bb7e4407872e0d6888eda8
Viktor Filippov <mozgfoxtrot@gmail.com>
2023-07-11 03:15:03 +0400

    just idk

--- controllers/cards.js
+++ controllers/cards.js
@@ -48,6 +48,7 @@ function likeCard(req, res) {
       { $addToSet: { likes: req.user._id } },
       { new: true },
     )
+    .orFail()
     .then((card) => res.status(200).send(card))
     .catch((err) => {
       if (err.name === 'CastError') {
@@ -69,6 +70,7 @@ function dislikeCard(req, res) {
       { $pull: { likes: req.user._id } },
       { new: true },
     )
+    .orFail()
     .then((card) => res.status(200).send(card))
     .catch((err) => {
       if (err.name === 'CastError') {
