3bc3c652f80e2e2ff6f159d9dd3ef24255698c1f
Viktor Filippov <mozgfoxtrot@gmail.com>
2023-07-11 14:12:36 +0400

    add path * for 404

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.use((req, res, next) => {
 app.use(userRoutes);
 app.use(cardRoutes);
 
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена.' });
+});
+
 app.listen(PORT, () => {
   console.log(`Application is running on port ${PORT}`);
 });
