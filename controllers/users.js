8d9c280e8e13ba966824f503c2a65c4cf037d7cb
Viktor Filippov <mozgfoxtrot@gmail.com>
2023-07-20 22:34:28 +0400

    create errors Classes + fix user controller + fix createUser func

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,16 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const userSchema = require('../models/users');
+const ConflictError = require('../errors/ConflictError');
+const BadRequestError = require('../errors/BadRequestError');
+const NotFoundError = require('../errors/NotFoundError');
 
 function getUsers(req, res, next) {
   return userSchema
     .find({})
     .then((users) => res.status(200).send(users))
-    .catch(() => {
-      next({ message: 'На сервере произошла ошибка' });
+    .catch((err) => {
+      next(err);
     });
 }
 
@@ -16,41 +19,50 @@ function getUserById(req, res, next) {
     .findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        next({ message: 'Нет пользователя с таким id' });
+        throw new NotFoundError('Нет пользователя с таким id');
       }
       return res.status(200).send(user);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        next({ message: 'Переданы некорректные данные' });
+        throw new BadRequestError('Переданы некорректные данные');
       } else {
-        next({ message: 'На сервере произошла ошибка' });
+        next(err);
       }
     });
 }
 
 function createUser(req, res, next) {
-  const { name, about, avatar, email, password } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
 
-  bcrypt
-    .hash(password, 10)
-    .then((hash) => {
-      userSchema.create({
-        name,
-        about,
-        avatar,
-        email,
-        password: hash,
-      });
+  if (!email || !password) {
+    throw new BadRequestError('Переданы некорректные данные');
+  }
+
+  userSchema
+    .findOne({ email })
+    .then((user) => {
+      if (user) {
+        throw new ConflictError(`Пользователь с таким ${email} уже существует`);
+      }
+      bcrypt.hash(password, 10);
     })
-    .then((user) => res.status(201).send(user))
+    .then((hash) => userSchema.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
+    .then((user) => userSchema.findOne({ _id: user._id }))
+    .then((user) => res.send({ user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next({
-          message: 'Переданы некорректные данные при создании пользователя.',
-        });
+        throw new BadRequestError('Некорректные данные');
       } else {
-        next({ message: 'На сервере произошла ошибка' });
+        next(err);
       }
     });
 }
@@ -62,20 +74,19 @@ function updateUser(req, res, next) {
     .findByIdAndUpdate(
       owner,
       { name, about },
-      { new: true, runValidators: true }
+      { new: true, runValidators: true },
     )
     .then((user) => {
       if (!user) {
-        next({ message: 'Нет пользователя с таким id' });
+        throw new NotFoundError('Нет пользователя с таким id');
       }
       return res.status(200).send(user);
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next({ message: 'Переданы некорректные данные' });
-      } else {
-        next({ message: 'На сервере произошла ошибка' });
+        throw new BadRequestError('Переданы некорректные данные');
       }
+      next(err);
     });
 }
 
@@ -86,11 +97,11 @@ function updateAvatar(req, res, next) {
     .findByIdAndUpdate(owner, { avatar }, { new: true })
     .then((user) => {
       if (!user) {
-        next({ message: 'Нет пользователя с таким id' });
+        throw new NotFoundError('Нет пользователя с таким id');
       }
       return res.status(200).send(user);
     })
-    .catch(() => next({ message: 'На сервере произошла ошибка' }));
+    .catch((err) => next(err));
 }
 
 function login(req, res, next) {
@@ -111,16 +122,15 @@ function getCurrentUser(req, res, next) {
     .findById(req.user._id)
     .then((user) => {
       if (!user) {
-        next({ message: 'Пользователь не найден' });
+        throw new NotFoundError('Пользователь не найден');
       }
       return res.status(200).send(user);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        next({ message: 'Переданы некорректные данные' });
-      } else {
-        next({ message: 'На сервере произошла ошибка' });
+        throw new BadRequestError('Переданы некорректные данные');
       }
+      next(err);
     });
 }
 
